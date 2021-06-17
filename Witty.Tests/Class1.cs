5f68eb2b3e38262f32e1eba4aa834f9db1023cbc
Dominic <dominicjosephenriquez@gmail.com>
2021-06-17 22:38:57 +0200

    Added Nunit test project

---
+++ Witty.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Witty.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void nothing()
+        {
+        }
+    }
+}
